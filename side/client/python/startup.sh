5597fa9d76ac1795f716080a1b699da9d9952c4f
BinaryBlazer <janjytapyt@sdevs.org>
2023-09-06 17:54:28 +0200

    Update startup.sh
    Signed-off-by: BinaryBlazer <janjytapyt@sdevs.org>

--- side/client/python/startup.sh
+++ side/client/python/startup.sh
@@ -1,1 +1,13 @@
-# soon
+if [[ -d .git ]] && [[ "{{AUTO_UPDATE}}" == "1" ]]; then
+    git pull
+fi
+
+if [[ ! -z "{{PY_PACKAGES}}" ]]; then
+    pip install -U --prefix .local {{PY_PACKAGES}}
+fi
+
+if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then
+    pip install -U --prefix .local -r ${REQUIREMENTS_FILE}
+fi
+
+/usr/local/bin/python /home/container/{{PY_FILE}}
