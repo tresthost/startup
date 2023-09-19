a76eab0c992887d0ca45272cc6b7eda665fe4ed9
BinaryBlazer <janjytapyt@sdevs.org>
2023-09-19 18:04:02 +0200

    [✅] fixed latest .tresthost core updates.
    Signed-off-by: BinaryBlazer <janjytapyt@sdevs.org>

--- side/client/python/startup.sh
+++ side/client/python/startup.sh
@@ -1,13 +1,13 @@
-if [[ -d .git ]] && [[ "{{AUTO_UPDATE}}" == "1" ]]; then
+if [[ -d .git && $AUTO_UPDATE == "1" ]]; then
     git pull
 fi
 
-if [[ ! -z "{{PY_PACKAGES}}" ]]; then
-    pip install -U --prefix .local {{PY_PACKAGES}}
+if [[ ! -z $PY_PACKAGES ]]; then
+    pip install -U --prefix .local $PY_PACKAGES
 fi
 
-if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then
-    pip install -U --prefix .local -r ${REQUIREMENTS_FILE}
+if [[ -f /home/container/$REQUIREMENTS_FILE ]]; then
+    pip install -U --prefix .local -r $REQUIREMENTS_FILE
 fi
 
-/usr/local/bin/python /home/container/{{PY_FILE}}
+/usr/local/bin/python /home/container/$PY_FILE
